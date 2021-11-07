fd33d6392ec2d238b2373910dae051d1796afcf1
Shafiqul Islam <shafiqul@sheba.xyz>
2021-11-07 20:17:51 +0600

    ⬆️  New wallpaper
    Type(s): UPDATE

--- zsh/zshrc/paths.zsh
+++ zsh/zshrc/paths.zsh
@@ -6,11 +6,14 @@ export GRADLE_HOME=/opt/gradle/gradle-5.6
 export PATH=${GRADLE_HOME}/bin:${PATH}
 
 # Export the Android SDK path 
-export ANDROID_HOME=$HOME/android-sdk
+export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/build-tools/28.0.3
 #export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
 
 export PATH=$PATH:$HOME/.composer/vendor/bin
 export PATH=$PATH:$(ruby -e 'puts Gem.bindir')
+
+export PATH="$PATH:$HOME/.dotnet/tools/"
