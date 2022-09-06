5539d68cc79d25965830dfff24eea78ad58000b3
Shafiqul Islam <shafiqul@sheba.xyz>
2022-09-06 14:37:52 +0600

    ➕ Add games
    Type(s): ADD

---
+++ games/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install bastet
+sudo apt-get install pacman4console
+sudo apt-get install nsnake
+wget https://raw.githubusercontent.com/mevdschee/2048.c/master/2048.c
+gcc -o 2048 2048.c
--- zsh/zshrc/aliases.zsh
+++ zsh/zshrc/aliases.zsh
@@ -3,6 +3,7 @@ alias sudo='sudo '
 alias ll='colorls -lA --sd --group-directories-first'
 alias ls='colorls --group-directories-first'
 alias cat='bat --paging=never'
+alias 2048="cd $DOTFILES_DIR/games && ./2048"
 
 # machine specific aliases
 [[ ! -f ~/.aliases.sh ]] || source ~/.aliases.sh
